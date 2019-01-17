eaa6681e58c85e592564fc46b503c79d92f92e4b
Pietro Stroia <pstroia@vmware.com>
2019-01-17 09:21:56 -0800

    fix paths coming from microdump.lua

--- microdump.lua
+++ microdump.lua
@@ -1,10 +1,10 @@
--- Copyright 2018 VMware, Inc.
+-- Copyright 2019 VMware, Inc.
 -- SPDX-License-Identifier: BSD-2-Clause
 
 
 -- chisel description
 description = "this chisel will print all the file paths included in your capture, including relative commands to PATH"
-short_description = "get a list a files to build a microcontainer, use with micropacker.py"
+short_description = "get a list a files to build a microcontainer, use with ./micropacker"
 category = "misc"
 
 
@@ -15,32 +15,31 @@ args = {}
 -- Initialization callback
 function on_init()
 	-- get some handlers
-	exec_h = chisel.request_field("evt.arg.exe")
-	file_h = chisel.request_field("evt.arg.filename")
-	name_h = chisel.request_field("evt.arg.name")
-	path_h = chisel.request_field("evt.arg.path")
+	dir_h = chisel.request_field("evt.dir")
+	stype_h = chisel.request_field("syscall.type")
+	args_h = chisel.request_field("evt.args")
+	abs_h = chisel.request_field("evt.abspath")
 	return true
 end
 
 
 -- main event handler
 function on_event()
-	-- get the fields, then print in an if condition, we want to be fast here
-	exec = evt.field(exec_h)
-	file = evt.field(file_h) 
-	name = evt.field(name_h)
-	path = evt.field(path_h)
-	if exec ~= nil then
-		print(exec)
-	end
-	if file ~= nil then
-		print(file)
-	end
-	if name ~= nil then
-		print(name)
-	end	
-	if path ~= nil then
-		print(path)
+	-- get the fields if direction is right
+	if evt.field(dir_h) == ">" and evt.field(stype_h) ~= nil then
+		abs_path = evt.field(abs_h)
+		if abs_path ~= nil then
+			print(abs_path)
+		else
+			args = evt.field(args_h)
+			if args.filename ~= nil then
+				print(args.filename)
+			elseif args.name ~= nil then
+				print(args.name)
+			elseif args.path ~= nil then
+				print(args.path)
+			end
+		end
 	end
 	return true
 end
